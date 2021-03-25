0e4309fff40551a44c363af73d2a787a2d884465
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2021-03-25 22:53:22 +0300

    First iteration animation of quots.

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -20,3 +20,4 @@ import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 // import "../../node_modules/bootstrap/js/dist/util.js";
 // import "../../node_modules/bootstrap/js/dist/modal.js";
+
